a6de3c3c2a0d10cefcf654a576879f273947f388
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2018-10-17 22:17:55 -0300

    Update agent.cc

--- prisoner_dilema/src/agent.cc
+++ prisoner_dilema/src/agent.cc
@@ -7,9 +7,9 @@ Agent::Agent(repast::AgentId id, double newC, double newTotal): id_(id), c(newC)
 Agent::~Agent(){ }
 
 void Agent::set(uint32_t currentRank, double newC, double newTotal){
-    id_.currentRank(currentRank);
-    c     = newC;
-    total = newTotal;
+	id_.currentRank(currentRank);
+	c     = newC;
+	total = newTotal;
 }
 
 bool Agent::cooperate(){
@@ -17,13 +17,13 @@ bool Agent::cooperate(){
 }
 
 void Agent::play(repast::SharedContext<Agent>* context){
-    std::vector<Agent*> agentsToPlay;
+	std::vector<Agent*> agentsToPlay;
 	
 	agentsToPlay.push_back(this);
-    context->selectAgents(3, agentsToPlay, true);
+	context->selectAgents(3, agentsToPlay, true);
 	
-    double cPayoff     = 0;
-    double totalPayoff = 0;
+	double cPayoff     = 0;
+	double totalPayoff = 0;
     
     
 	for( auto agentToPlay : agentsToPlay ){
@@ -41,17 +41,17 @@ void Agent::play(repast::SharedContext<Agent>* context){
 		agentToPlay++;
 		
 	}
-    c      += cPayoff;
-    total  += totalPayoff;
+	c      += cPayoff;
+	total  += totalPayoff;
 	
 	/*	
 	std::set<Agent*> agentsToPlay;
 	
-    agentsToPlay.insert(this); // Prohibit playing against self	
-    context->selectAgents(3, agentsToPlay, true);
+	agentsToPlay.insert(this); // Prohibit playing against self	
+	context->selectAgents(3, agentsToPlay, true);
 	
-    double cPayoff     = 0;
-    double totalPayoff = 0;
+	double cPayoff     = 0;
+	double totalPayoff = 0;
 	
 	std::set<Agent*>::iterator agentToPlay = agentsToPlay.begin();
 	auto agentToPlay = agentsToPlay.begin();
@@ -62,57 +62,20 @@ void Agent::play(repast::SharedContext<Agent>* context){
 		
 		bool iCooperated = cooperate();                          // Do I cooperate?
 		double payoff = (iCooperated ?
-						 ((*agentToPlay)->cooperate() ?  7 : 1) :     // If I cooperated, did my opponent?
-						 ((*agentToPlay)->cooperate() ? 10 : 3));     // If I didn't cooperate, did my opponent?
+				((*agentToPlay)->cooperate() ?  7 : 1) :     // If I cooperated, did my opponent?
+				((*agentToPlay)->cooperate() ? 10 : 3));     // If I didn't cooperate, did my opponent?
 						 
 		if(iCooperated) cPayoff += payoff;
 		totalPayoff             += payoff;
 		
 		agentToPlay++;
-    }
-    c      += cPayoff;
-    total  += totalPayoff;
+	}
+	c      += cPayoff;
+	total  += totalPayoff;
 	*/
     
 	
 }
-
-/*void Agent::play(repast::SharedContext<Agent>* context){
-    std::set<Agent*> agentsToPlay;
-	
-	Agent* currAgent = this;
-	
-    agentsToPlay.insert(this); // Prohibit playing against self	
-    context->selectAgents(3, agentsToPlay, true);
-	
-    double cPayoff     = 0;
-    double totalPayoff = 0;
-    
-	//std::set<Agent*>::iterator agentToPlay = agentsToPlay.begin();
-	auto agentToPlay = agentsToPlay.begin();
-    
-	//for( fooAgent : agentsToPlay){
-	
-	while(agentToPlay != agentsToPlay.end()){
-		//double currTick = repast::RepastProcess::instance()->getScheduleRunner().currentTick();
-		//std::cout << currTick << ":\t" << this->getId() << "->" << (*agentToPlay)->getId() << std::endl;
-		
-		bool iCooperated = cooperate();                          // Do I cooperate?
-		double payoff = (iCooperated ?
-						 ((*agentToPlay)->cooperate() ?  7 : 1) :     // If I cooperated, did my opponent?
-						 ((*agentToPlay)->cooperate() ? 10 : 3));     // If I didn't cooperate, did my opponent?
-						 
-		if(iCooperated) cPayoff += payoff;
-		totalPayoff             += payoff;
-		
-		agentToPlay++;
-    }
-    c      += cPayoff;
-    total  += totalPayoff;
-	
-}*/
-
-
 
 /* Serializable Agent Package Data */
 
